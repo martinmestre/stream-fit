3ed76ef410ff228782ab57c50ea2cd5efb3680f2
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2022-06-27 15:13:55 -0300

    Update in pot_rot_barions_model.

---
+++ pot_rot_barions_model/README.md
@@ -0,0 +1,16 @@
+# Optimization of potential according to GD-1 stream and rotation curve.
+
+## Potential = Dark Matter (RAR) + barions (bulge + two disks)
+## Parameters: 2D-slice for RAR and 6D for the two disks.
+## Chi^2 = Chi^2(Stream)+weight*Chi^2(rotation curve)
+## Tests with four different values of weight.
+
+## Runs in 3 different computers:
+
+### antikythera: maxiter=30, popsize=50
+### laruidosa:   maxiter=100, popsize=200
+### serafin:     maxiter=100, popsize=200
+
+
+
+
---
+++ pot_rot_barions_model/potential_classes.py
@@ -0,0 +1,76 @@
+"""Potential classes."""
+
+# Gravitational Potential Classes.
+import config as cfg
+import numpy as np
+import model_def
+from scipy.interpolate import InterpolatedUnivariateSpline
+
+g = cfg.g
+
+
+class MiyamotoNagai:
+    """Miyamoto-Nagai disk."""
+
+    def __init__(self, M, a, b):
+        """Init."""
+        self.M = M
+        self.a = a
+        self.b = b
+
+    def accel(self, x, y, z):
+        """Acceleration."""
+        fac = -g*self.M / (x*x + y*y + (self.a + np.sqrt(self.b**2 + z*z))**2)**(1.5)
+        return np.array([fac*x, fac*y, fac*z*(1.0+self.a/np.sqrt(self.b**2+z*z))])
+
+
+class Plummer:
+    """Plummer sphere."""
+
+    def __init__(self, M, b):
+        """Init."""
+        self.M = M
+        self.b = b
+
+    def accel(self, x, y, z):
+        """Acceleration."""
+        fac = -g*self.M/(self.b**2+x*x+y*y+z*z)**(1.5)
+        return np.array([fac*x, fac*y, fac*z])
+
+
+class RAR:
+    """RAR halo."""
+
+    def __init__(self, param):
+        """Init."""
+        self.param = param
+        r, mass, nu = model_def.model(self.param)
+        isnan = np.argwhere(np.isnan(mass))
+        if (np.any(isnan)):
+            k = np.argwhere(np.isnan(mass))[0][0]
+        else:
+            k = -1
+        r_s = r[0:k]
+        mass_s = mass[0:k]
+        nu_s = nu[0:k]
+        self.r_max = np.amax(r_s)
+        self.mass_spline = InterpolatedUnivariateSpline(r_s, mass_s, k=4)
+        self.nu_spline = InterpolatedUnivariateSpline(r_s, nu_s, k=4)
+
+    def mass_wrap(self, r):
+        """Wrap."""
+        if(r < self.r_max):
+            return self.mass_spline(r)
+        else:
+            return self.mass_spline(self.r_max)
+
+    def accel(self, x, y, z):
+        """Acceleration."""
+        r = np.sqrt(x*x+y*y+z*z)
+        fac = -g*self.mass_wrap(r)/r**3
+        return fac*np.array([x, y, z])
+
+    def dnu(self, r):
+        """Derive nu."""
+        deriv = self.nu_spline.derivative(1)
+        return deriv(r)
