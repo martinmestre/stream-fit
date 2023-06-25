69db2c006516c3c8bb58db5c32630a812270c40c
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-06-25 11:00:09 -0300

    Another try with this file.

--- pipeline_paper/optim_polish_chi2full.jl
+++ pipeline_paper/optim_polish_chi2full.jl
@@ -38,33 +38,33 @@ function χ²Full(x, p)
     return stream.chi2_full(θ, Δθ, β, ϵ, ic, r☼)
 end
 
-# %%
-# Initial conditions from Galpy's MWPotential2014 model (param_fit_I-M-GaiaDR2_to_MWPot2014wGalpy.txt):
-ic = [149.16883497, 36.50694499, 7.94771853, -6.94192661, -12.48844894, -18.32838854]
-r☼ = 8.122
+function main()
+    # %%
+    # Initial conditions from Galpy's MWPotential2014 model (param_fit_I-M-GaiaDR2_to_MWPot2014wGalpy.txt):
+    ic = [149.16883497, 36.50694499, 7.94771853, -6.94192661, -12.48844894, -18.32838854]
+    r☼ = 8.122
+    #36.224542166107774, 27.46389908753495, 1.2472408164492195
+    θ₀, Δθ₀, βᵣ₀ =  3.612769971239480782e+01, 2.739433669170758989e+01, 1.2e-5
+    ϵ = 56.0
+    # %%
 
-θ₀, Δθ₀, βᵣ₀ =  3.612769971239480782e+01, 2.739433669170758989e+01, 1.2
-ϵ = 56.0
-# %%
-outfile = "param_optim_polish_chi2full.txt"
-
-p = [ϵ]
-x₀ = [θ₀, Δθ₀, βᵣ₀]
-lb = [0.9θ₀, 0.9Δθ₀, 0.9βᵣ₀]
-ub = [1.1θ₀, 1.1Δθ₀, 1.3βᵣ₀]
-prob = OptimizationProblem(χ²Full, x₀, p, ic=ic, r☼=r☼, lb=lb, ub=ub)
-sol = solve(prob, NLopt.LN_NELDERMEAD(), reltol=5.0e-5)
-x₀ = sol.u
-χ² = sol.minimum
-θ₀ = x₀[1]
-Δθ₀ = x₀[2]
-βᵣ₀ = x₀[3]
-χ²s = stream.chi2_stream(θ₀, Δθ₀, βᵣ₀*1.e-5, ϵ, ic, r☼)
-χ²c = stream.chi2_core(θ₀, Δθ₀, βᵣ₀*1.e-5, ϵ)
-println(f, ϵ, "  ", x₀[1], "  ", x₀[2], "  ", x₀[3] * 1.e-5, "  ", χ², "  ",  χ²s, "  ", χ²c)
-println("χ²:F-S-C:", ϵ, "  ", x₀[1], "  ", x₀[2], "  ", x₀[3] * 1.e-5,
-        "  ", χ², "  ",  χ²s, "  ", χ²c)
+    p = [ϵ]
+    x₀ = [θ₀, Δθ₀, βᵣ₀]
+    lb = [0.9θ₀, 0.9Δθ₀, 0.9βᵣ₀]
+    ub = [1.1θ₀, 1.1Δθ₀, 1.1βᵣ₀]
+    prob = OptimizationProblem(χ²Full, x₀, p, ic=ic, r☼=r☼, lb=lb, ub=ub)
+    sol = solve(prob, NLopt.LN_NELDERMEAD(), reltol=5.0e-5)
+    x₀ = sol.u
+    χ² = sol.minimum
+    θ₀ = x₀[1]
+    Δθ₀ = x₀[2]
+    βᵣ₀ = x₀[3]
+    χ²s = stream.chi2_stream(θ₀, Δθ₀, βᵣ₀*1.e-5, ϵ, ic, r☼)
+    χ²c = stream.chi2_core(θ₀, Δθ₀, βᵣ₀*1.e-5, ϵ)
+    write(outfile, ϵ, "  ", x₀[1], "  ", x₀[2], "  ", x₀[3] * 1.e-5, "  ", χ², "  ",  χ²s, "  ", χ²c)
+    println("χ²:F-S-C:", ϵ, "  ", x₀[1], "  ", x₀[2], "  ", x₀[3] * 1.e-5,
+            "  ", χ², "  ",  χ²s, "  ", χ²c)
+end
 
 
-
-
+main()
