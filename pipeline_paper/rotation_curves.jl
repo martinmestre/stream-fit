153bb5810dc8e733781a6233f7eba5f91cb21e50
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-07-06 20:23:02 -0300

    RotationCurve+

--- pipeline_paper/rotation_curves.jl
+++ pipeline_paper/rotation_curves.jl
@@ -31,7 +31,7 @@ pushfirst!(PyVector(pyimport("sys")."path"), "")
 importLib = pyimport("importlib")
 stream = pyimport("stream")
 potentials = pyimport("potential_classes")
-galpy_pots =  pyimport("galpy.potential")
+galpy_pots = pyimport("galpy.potential")
 u = pyimport("astropy.units")
 importLib.reload(stream)
 
@@ -41,7 +41,7 @@ param_file = "param_fit_pot_from_IbataPolysGaiaDR2_chi2full.txt"
 θ, ω, β = readdlm(param_file)
 
 ϵ = 56.0
-W = θ+ω
+W = θ + ω
 param = [ϵ, θ, W, β]
 
 
@@ -71,70 +71,71 @@ end
 v_nfw = [malhan_vcirc(x) for x in r];
 df_models = DataFrame(r=r, v_nfw=v_nfw, v_f=v_f)
 
- # %%
- # Reading rotation curve observables
- # Set rotation data
-
- df_Sof13 = DataFrame(CSV.File("data_rotcurves/vel_Sofue13.txt"; delim=" ", ignorerepeated=true))
- df_Sof20 = DataFrame(CSV.File("data_rotcurves/vel_Sofue20.txt"; delim=" ", ignorerepeated=true))
- df_Eilers = DataFrame(CSV.File("data_rotcurves/vel_Eilers.txt"; delim=" ", ignorerepeated=true))
-# asym_error = DataFrame([v_Eilers.e_down, v_Eilers.e_up])
-df_Sof13.r = df_Sof13.r/1.0e3
-df_Sof13.err_r =  df_Sof13.err_r/1.0e3
-df_Sof13
-
 # %%
-labels = ["Fermionic-MW","NFW-MW"]
-lw = 4
+# Reading rotation curve observables
+# Set rotation data
+
+df_Sof13 = DataFrame(CSV.File("data_rotcurves/vel_Sofue13.txt"; delim=" ", ignorerepeated=true))
+df_Sof20 = DataFrame(CSV.File("data_rotcurves/vel_Sofue20.txt"; delim=" ", ignorerepeated=true))
+df_Eilers = DataFrame(CSV.File("data_rotcurves/vel_Eilers.txt"; delim=" ", ignorerepeated=true))
+# asym_error = DataFrame([v_Eilers.e_down, v_Eilers.e_up])
+df_Sof13.r = df_Sof13.r / 1.0e3
+df_Sof13.err_r = df_Sof13.err_r / 1.0e3
+df_Sof20
+
 # %%
 
 # Plot
-let
-      set_aog_theme!()
-      size_inches = (6.2*2, 3*2)
-      size_pt = 72 .* size_inches
-      fig = Figure(resolution = size_pt, fontsize = 37)
-      gridpos = fig[1, 1]
-      grp = dims(1) => renamer(labels) => ""
-      plt = data(df_models) *
-          mapping(:r => L"r~[\textrm{kpc}]", [2,3] .=> L"v_{\textrm{circ}}~[\textrm{km s}^{-1}]";
-              color = grp,
-              linestyle = grp
-          ) *
-          visual(Lines, linewidth=lw)
-      errorMap = mapping(:r, :v, (:r, :err_r) => ((x,y) -> x - y),
-          (:r, :err_r) => ((x,y) -> x + y),
-          ) * visual(Violin,color = (:grey, 0.7));
-      plt_Sof13 = data(df_Sof13)*errorMap
-      f = draw!(gridpos, plt+plt_Sof13, axis=(;limits=((0,40),(0,300)),
-          xgridvisible=false, ygridvisible=false))
-      # ax = f[1,1].axis
-      # errorbars!(ax,df_Sof13.r, df_Sof13.v, df_Sof13.err_r; whiskerwidth = 10, direction=:x)
-      # errorbars!(ax,df_Sof13.r, df_Sof13.v, df_Sof13.err_v; whiskerwidth = 10, direction=:y)
-      legend!(gridpos, f; tellwidth=false, halign=:center, valign=:bottom, margin=(10, 10, 10, 10), patchsize=(50,35))
-      # Lines re-styling
-      amber_aog = "#ffa700"
-      green_aog = "#107A78"
-      lineas = fig.content[1].scene.plots
-      lineas[1].linestyle = :dash
-      lineas[2].linestyle = :dot
-      lineas[1].color = amber_aog
-      lineas[2].color = green_aog
+labels = ["Fermionic-MW", "NFW-MW"]
+lw = 4
 
-      leg = fig.content[2]
-      _lines = leg.blockscene.children[1].plots[2:3]
-      for l in _lines
-            l.linewidth = 4
-      end
-      _lines[1].linestyle = :dash
-      _lines[2].linestyle = :dot
-      _lines[1].color = amber_aog
-      _lines[2].color = green_aog
+#let
+set_aog_theme!()
+size_inches = (6.2 * 2, 3 * 2)
+size_pt = 72 .* size_inches
+fig = Figure(resolution=size_pt, fontsize=37)
+gridpos = fig[1, 1]
+grp = dims(1) => renamer(labels) => ""
+grp2 = dims(1) => renamer(["Sofue"]) => ""
+plt_model = data(df_models) *
+        mapping(:r => L"r~[\textrm{kpc}]", [2, 3] .=> L"v_{\textrm{circ}}~[\textrm{km s}^{-1}]";
+            color=grp,
+            linestyle=grp
+        ) *
+        visual(Lines, linewidth=lw)
+plt_Sof13 = data(df_Sof13) * mapping(:r, :v) * visual(Scatter, direction=:x)
+plt_eb_Sof13 = data(df_Sof13) * (mapping(:r, :v, :err_r) * visual(Errorbars, direction=:x) + mapping(:r, :v, :err_v) * visual(Errorbars))
+plt_eb_Sof20 = data(df_Sof20) * mapping(:Radius, :Velocity, :Error) * visual(Errorbars)
+plt_full = plt_model+plt_Sof13+plt_eb_Sof13+plt_eb_Sof20
+f = draw!(gridpos, plt_full, axis=(; limits=((0, 40), (0, 300)),
+    xgridvisible=false, ygridvisible=false))
+legend!(gridpos, f; tellwidth=false, halign=:center, valign=:bottom, margin=(10, 10, 10, 10), patchsize=(50, 35))
+# Lines re-styling
+amber_aog = "#ffa700"
+green_aog = "#107A78"
+lineas = fig.content[1].scene.plots
+lineas[1].linestyle = :dash
+lineas[2].linestyle = :dot
+lineas[1].color = amber_aog
+lineas[2].color = green_aog
+lineas[3].color = :blue
 
-      display(fig)
-      save("paper_plots/rotation_curves.pdf", fig, pt_per_unit = 1)
-      println("plot done.")
+leg = fig.content[2]
+_lines = leg.blockscene.children[1].plots[2:3]
+for l in _lines
+    l.linewidth = 4
 end
+_lines[1].linestyle = :dash
+_lines[2].linestyle = :dot
+_lines[1].color = amber_aog
+_lines[2].color = green_aog
+
+display(fig)
+save("paper_plots/rotation_curves.pdf", fig, pt_per_unit=1)
+println("plot done.")
+#end
 
 # %%
-df_Sof13.err_v
+leg.blockscene.children[1].plots[4]
+
+# %%
