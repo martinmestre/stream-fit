bb4b877822b9f57c12841663fa4fa7af6193afdc
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-07-14 10:58:05 -0300

    Contour plot of chi2stream function done correctly. Text updated with chi2stream plot and the shaded regions in the other plots. Remains to do the plots of variation of fermion mass.

--- pipeline_paper/plot_chi2stream.jl
+++ pipeline_paper/plot_chi2stream.jl
@@ -31,18 +31,20 @@ stream = pyimport("stream")
 
 
 # Open input file
-# chi2_file = "chi2stream_beta0_1.258e-05.txt"
-chi2_file = "laruidosa/likelihood_beta0_1.258e-05.txt"
-matriz = readdlm(chi2_file);
 
+chi2_file = "laruidosa/likelihood_beta0_1.258e-05.txt"
+chi2_t_file = "chi2stream_tilted_beta0_1.258e-05.txt"
+matriz = readdlm(chi2_file);
+matriz_t = readdlm(chi2_t_file);
 
 
 
 # %%
+# Loading elements from the original run
 θ = matriz[:, 1]
 ω = matriz[:, 2]
 β = matriz[:, 3]
-#χ²= matriz[:,4] # for chi2stream file
+# χ²= matriz[:,4] # for chi2stream file
 χ² = -matriz[:, 4]; #for likelihood file
 
 
@@ -61,6 +63,28 @@ fig = draw(plt + plt_crest, axis=(xlabel=L"θ_0", ylabel=L"ω_0"))
 scatter!(sol, color=:black, markersize=15)
 save("paper_plots/chi2stream.pdf", fig, pt_per_unit=1)
 fig
+
+# %%
+set_aog_theme!()
+update_theme!(fontsize=40)
+
+
+f = Figure()
+ax=Axis(f[1, 1], xlabel=L"θ_0", ylabel=L"ω_0", limits=((34,38),(25.5,28.2)))
+levels = collect(range(1000.,10000.,length=10))
+pushfirst!(levels,500)
+pushfirst!(levels, 20)
+co = contourf!(θ, ω, χ², levels=levels,
+               extendhigh=(:black,0.2),colormap=:viridis)
+Colorbar(f[1, 2], co;ticks=levels,
+        ticklabelsize=25, label=L"χ²_{\textrm{stream}}")
+sol = (36.0661, 27.3468)
+scatter!(sol, color=:black, markersize=15)
+save("paper_plots/chi2stream_contourf.pdf", f, pt_per_unit=1)
+f
+
+# %%
+collect(range(1000.,10000.,length=10))
 
 # %%
 # Linear Fit to the valley.
@@ -87,27 +111,47 @@ save("paper_plots/chi2stream_tilted.pdf", fig, pt_per_unit=1)
 display(fig)
 
 # %%
+# Loading elements from the tilted run
+θ = matriz_t[:, 1]
+ξ = matriz_t[:, 2]
+β = matriz_t[:, 3]
+χ²= matriz_t[:, 4]; # for chi2stream file
+
+
+# %%
 # Looking at the Likelihood in the parameter 2D slice (tilted)
+# I haven't managed to make this work with AoG. See cell below with plain Makie.
 
 set_aog_theme!()
-size_inches = (6.2*2, 4*2)
-size_pt = 72 .* size_inches
-fig = Figure(resolution = size_pt, fontsize = 40)
-
-η = ω - h.(θ)
-bool = χ² .< 1000
-df_tilt = (x=θ[bool], y=η[bool], z=χ²[bool])
-plt = data(df_tilt) * mapping(:x, :y; color=:z => L"χ²_{\textrm{stream}}") * (visual(Scatter, markersize=10, colormap=:viridis))
-bool = 40 .< χ² .< 50
-df_crest = (x=θ[bool], y=η[bool], z=χ²[bool])
-plt_crest = data(df_crest) * mapping(:x, :y) * visual(Scatter, markersize=10, color=:red)
+update_theme!(fontsize=37)
+df_tilt = (x=θ, y=ξ, z=χ²)
+plt = data(df_tilt) * mapping(:x, :y; color=:z => L"χ²_{\textrm{stream}}") * (visual(Scatter, markersize=3, colormap=:viridis))
+plt_crest = data(df_crest) * mapping(:x, :y) * visual(Scatter, markersize=1, color=:red)
 sol = (36.0661, 27.3468- h(36.0661))
-fig = Figure(resolution = size_pt, fontsize = 37)
-gridpos = fig[1, 1]
-f = draw!(gridpos,plt+plt_crest, axis=(xlabel=L"θ_0", ylabel=L"ω_0-h(θ_0)",limits=((35,37),(sol[2]-0.02, sol[2]+0.02))))
-scatter!(sol, color=:black, markersize=25)
+plt_contour =  data(df_tilt) * mapping(:x, :y, :z)*visual(Contourf,levels=[19.,19.2, 19.32, 30, 50,100,200,300,500], colormap=:viridis)
 
+fig = Figure(fontsize = 40)
+gridpos = fig[1, 1]
+f = draw!(gridpos, plt_contour, axis=(xlabel=L"θ_0", ylabel=L"ω_0-h(θ_0)",limits=((35,37),(-0.01,0.01))))
+
+AlgebraOfGraphics.colorbar!(fig[1,2],f; ticks=[19.,19.2, 19.32, 30, 50,100,200,300,500])
 save("paper_plots/chi2stream_tilted.pdf", fig, pt_per_unit=1)
-display(fig)
+fig
+
+# %%
+set_aog_theme!()
+update_theme!(fontsize=40)
+
+
+f = Figure()
+ax=Axis(f[1, 1], xlabel=L"θ_0", ylabel=L"ω_0-h(θ_0)", limits=((35,37),(-0.011,0.011)))
+
+co = contourf!(θ, ξ, χ², levels=[19., 20, 30, 50, 70, 100,200,300,500],
+               extendhigh=(:black,0.2),colormap=:viridis)
+
+Colorbar(f[1, 2], co;ticks=[20, 50,70, 100,200,300,500],
+        ticklabelsize=25, label=L"χ²_{\textrm{stream}}")
+save("paper_plots/chi2stream_tilted_contourf.pdf", f, pt_per_unit=1)
+f
 
 # %%
