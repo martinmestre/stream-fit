e78fb4e39bd566ccb5adb512a2c40afe481d8d41
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-08-10 10:44:28 -0300

    Testing with Nomad.jl through Optimization.jl

--- pipeline_paper/optim_pot.jl
+++ pipeline_paper/optim_pot.jl
@@ -1,27 +1,9 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     custom_cell_magics: kql
-#     text_representation:
-#       extension: .jl
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
-#   kernelspec:
-#     display_name: Julia 1.9.0
-#     language: julia
-#     name: julia-1.9
-# ---
-
-# %%
 """Perform optimization for ϵ ∈ [56, 370] sequentially."""
 
 using Pkg
 Pkg.activate(".")
 using PyCall
-using Evolutionary
+using Optimization, OptimizationNOMAD
 using DelimitedFiles
 using CSV
 using DataFrames, DataFramesMeta
@@ -51,28 +33,34 @@ println("threads=", Threads.nthreads())
 """Loop in ϵ."""
 
 """χ² wrap."""
-function χ²Full(x)
+function χ²Full(x, p)
     θ = x[1]
     ω = x[2]
     β = x[3]/10^5
+    m = p[1]
+    ic = p[2]
+    r☼ = p[3]
     return stream.chi2_full(θ, ω, β, m, ic, r☼)
 end
 
 
 """Main function."""
-function main()
-    lb = [35, 37, 1.0]
-    ub = [25, 30, 1.5]
+function main(m, ic, r☼)
+    lb = [35., 26., 1.0]
+    ub = [37., 28., 1.5]
     x₀ = [36., 27., 1.25]
-    res = Evolutionary.optimize(χ²Full, x₀, DE(populationSize=100),
-                                Evolutionary.Options(iterations=100,
-                                                     abstol=5.e-5,
-                                                     reltol=5.e-5,
-                                                     parallelization=:thread))
+    p = [m, ic, r☼]
+    # sol = Evolutionary.optimize(χ²Full, x₀, DE(populationSize=100),
+    #                             Evolutionary.Options(iterations=100,
+    #                                                  abstol=5.e-5,
+    #                                                  reltol=5.e-5,
+    #                                                  parallelization=:thread))
+    prob = OptimizationProblem(χ²Full, x₀, p, lb=lb, ub=ub)
+    sol = Optimization.solve(prob, NOMADOpt(); reltol=5.e-5)
     # open(sol_file,"a") do io
     #     println(io, "xmin = $(xmin)")
     # end
-    return res
+    return sol
 end
 
 
@@ -80,8 +68,8 @@ end
 
 """Running."""
 m = 56.0
-res = main()
-display(res)
+sol = main(m, ic, r☼)
+display(sol)
 # %%
 
 
