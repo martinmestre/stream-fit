8a2a62f9171967ccd9ea28cfcd9d0de7dc2d9db9
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-08-11 11:05:45 -0300

    Runs in laruidosa for the five values of m.

--- pipeline_paper/optim_pot.jl
+++ pipeline_paper/optim_pot.jl
@@ -36,8 +36,16 @@ end
 
 """Main function."""
 function main(m, ic, r☼)
-    lb = [36., 27., 4.0e-5]
-    ub = [38., 29., 7.0e-5]
+    # lb = [41., 28., 0.001] # for m = 360
+    # ub = [45., 31., 0.005]
+    # lb = [39., 28., 0.0005]  # for m = 300
+    # ub = [42., 31., 0.003 ]
+    # lb = [38., 28., 0.0001] # for m = 200
+    # ub = [40., 30., 0.0006]
+    # lb = [36., 27., 4.0e-5] # for m=100
+    # ub = [38., 29., 7.0e-5]
+    lb = [34., 26., 1.0e-5] # for m=56
+    ub = [38., 30., 1.5e-5]
     x₀ = 0.5*(lb+ub)
     p = [m, ic, r☼]
     prob = OptimizationProblem(χ²Full, x₀, p, lb=lb, ub=ub)
@@ -54,11 +62,11 @@ const r☼ = 8.122
 # %%
 
 """Running."""
-const m = 100.0
+const m = 56.0
 const sol_file = "param_optim_pot_m$(Int(m)).txt"
-@show m
+@show m sol_file
 sol = main(m, ic, r☼)
-@show sol_file
+@show sol
 writedlm(sol_file, sol.u)
 # %%
 
