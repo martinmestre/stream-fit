76b5f37d728563fbcc7ca5129ddaf997c7813de8
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-08-22 17:44:39 -0300

    trying to make it run in dirac

--- pipeline_paper/optim_pot.jl
+++ pipeline_paper/optim_pot.jl
@@ -3,95 +3,98 @@ for a grid in a global region of paramter space.
 Using Distributed.jl
 """
 
+
 @everywhere begin
-
-using Pkg
-Pkg.activate(".")
-
-using PyCall
-using Optimization, OptimizationNOMAD
-using FiniteDiff
-using DelimitedFiles
-using CSV
-using DataFrames, DataFramesMeta
-using Distributed
-
-# %%
-pushfirst!(PyVector(pyimport("sys")."path"), "")
-importLib = pyimport("importlib")
-stream = pyimport("stream")
-potentials = pyimport("potential_classes")
-u = pyimport("astropy.units")
-importLib.reload(stream)
-importLib.reload(potentials)
-# %%
-
-# println("Threads=", Threads.nthreads())
-
-"""Loop in ϵ."""
-
-"""Anti-normalization function."""
-function back_orig(x, a, b)
-    return (b-a).*x + a
+    using Pkg
+    Pkg.activate(".")
+end
+@everywhere begin
+    using PyCall
+    using Optimization, OptimizationNOMAD
+    using FiniteDiff
+    using DelimitedFiles
+    using CSV
+    using DataFrames, DataFramesMeta
+    using Distributed
 end
 
-"""χ² wrap."""
-function χ²Full(x, p)
-    m = p[1]
-    ic = p[2]
-    r☼ = p[3]
-    θ, ω, β = x
-    return stream.chi2_full(θ, ω, β, m, ic, r☼)
+@everywhere begin
+    # %%
+    pushfirst!(PyVector(pyimport("sys")."path"), "")
+    importLib = pyimport("importlib")
+    stream = pyimport("stream")
+    potentials = pyimport("potential_classes")
+    u = pyimport("astropy.units")
+    # importLib.reload(stream)
+    # importLib.reload(potentials)
 end
+    # %%
+@everywhere begin
+    println("Threads=", Threads.nthreads())
+
+    """Loop in ϵ."""
+
+    """Anti-normalization function."""
+    function back_orig(x, a, b)
+        return (b-a).*x + a
+    end
+
+    """χ² wrap."""
+    function χ²Full(x, p)
+        m = p[1]
+        ic = p[2]
+        r☼ = p[3]
+        θ, ω, β = x
+        return stream.chi2_full(θ, ω, β, m, ic, r☼)
+    end
 
 
-"""Worker function."""
-function worker(i, sol_dir, m, ic, r☼, lb, ub)
-    x₀ = 0.5*(lb+ub)
-    p = (m, ic, r☼)
-    prob = OptimizationProblem(χ²Full, x₀, p, lb=lb, ub=ub)
-    sol = Optimization.solve(prob, NOMADOpt(); display_all_eval=false, maxiters=3)
-    worker_file = "$(sol_dir)/worker_optim_pot_m$(Int(m))_i$i.txt"
-    @show i, myid(), sol.u, χ², worker_file
-    worker_sol = ("Minimizer = $(sol.u)", "Minimum = $(sol.objective)")
-    writedlm(worker_file, worker_sol)
+    """Worker function."""
+    function worker(i, sol_dir, m, ic, r☼, lb, ub)
+        x₀ = 0.5*(lb+ub)
+        p = (m, ic, r☼)
+        prob = OptimizationProblem(χ²Full, x₀, p, lb=lb, ub=ub)
+        sol = Optimization.solve(prob, NOMADOpt(); display_all_eval=false, maxiters=3)
+        worker_file = "$(sol_dir)/worker_optim_pot_m$(Int(m))_i$i.txt"
+        @show i, myid(), sol.u, χ², worker_file
+        worker_sol = ("Minimizer = $(sol.u)", "Minimum = $(sol.objective)")
+        writedlm(worker_file, worker_sol)
 
-    return sol
-end
+        return sol
+    end
 
-"""Build grid."""
-function build_grid(lb_g, ub_g, n_grid)
-    n_full = n_grid^3
-    lb_a = Vector{Vector{Float64}}(undef,n_full)
-    ub_a = Vector{Vector{Float64}}(undef,n_full)
-    x₀_a = Vector{Vector{Float64}}(undef,n_full)
-    c₁ = collect(range(lb_g[1], ub_g[1], n_grid+1))
-    c₂ = collect(range(lb_g[2], ub_g[2], n_grid+1))
-    c₃ = collect(range(lb_g[3], ub_g[3], n_grid+1))
-    for i ∈ 1:n_grid
-        for j ∈ 1:n_grid
-            for k ∈ 1:n_grid
-                n = (i-1)*n_grid^2+(j-1)*n_grid+k
-                lb_a[n] = [c₁[i], c₂[j], c₃[k]]
-                ub_a[n] = [c₁[i+1], c₂[j+1], c₃[k+1]]
-                x₀_a[n] = 0.5*(lb_a[n]+ub_a[n])
+    """Build grid."""
+    function build_grid(lb_g, ub_g, n_grid)
+        n_full = n_grid^3
+        lb_a = Vector{Vector{Float64}}(undef,n_full)
+        ub_a = Vector{Vector{Float64}}(undef,n_full)
+        x₀_a = Vector{Vector{Float64}}(undef,n_full)
+        c₁ = collect(range(lb_g[1], ub_g[1], n_grid+1))
+        c₂ = collect(range(lb_g[2], ub_g[2], n_grid+1))
+        c₃ = collect(range(lb_g[3], ub_g[3], n_grid+1))
+        for i ∈ 1:n_grid
+            for j ∈ 1:n_grid
+                for k ∈ 1:n_grid
+                    n = (i-1)*n_grid^2+(j-1)*n_grid+k
+                    lb_a[n] = [c₁[i], c₂[j], c₃[k]]
+                    ub_a[n] = [c₁[i+1], c₂[j+1], c₃[k+1]]
+                    x₀_a[n] = 0.5*(lb_a[n]+ub_a[n])
+                end
             end
         end
+        return lb_a, ub_a, x₀_a
     end
-    return lb_a, ub_a, x₀_a
+
+
+    """Parallel function."""
+    function cooperative(sol_dir, m, ic, r☼, lb_g, ub_g, n_grid)
+        lb_a, ub_a, x₀_a = build_grid(lb_g, ub_g, n_grid)
+        pars(i) = [i, sol_dir, m, ic, r☼, lb_a[i], ub_a[i]]
+        res = pmap(i->worker(pars(i)...), eachindex(x₀_a))
+        return res
+    end
+    # %%
 end
-
-
-"""Parallel function."""
-function cooperative(sol_dir, m, ic, r☼, lb_g, ub_g, n_grid)
-    lb_a, ub_a, x₀_a = build_grid(lb_g, ub_g, n_grid)
-    pars(i) = [i, sol_dir, m, ic, r☼, lb_a[i], ub_a[i]]
-    res = pmap(i->worker(pars(i)...), eachindex(x₀_a))
-    return res
-end
-# %%
-
-end  # @everywhere
 
 
 """Initial orbit conditions file."""
@@ -104,14 +107,16 @@ const m = 360.0
 const sol_dir = "sol_dir_optim_pot_m$(Int(m))"
 const sol_file = "sol_optim_pot_m$(Int(m))"
 const r☼ = 8.122
-const lb = [40., 27., 1.e-5]
-const ub = [45., 31., 0.005]
+const lb_g = [40., 27., 1.e-5]
+const ub_g = [45., 31., 0.005]
 const n_grid = 10
 @show m sol_file r☼ lb_g ub_g
 
-run(`mkdir $sol_dir`)
+if !isdir(sol_dir)
+    run(`mkdir $sol_dir`)
+end
 
-"""Running."""
+# """Running."""
 sol = cooperative(sol_dir, m, ic, r☼, lb_g, ub_g, n_grid)
 @show sol
 obj = [sol[i].objective for i ∈ eachindex sol]
---
+++ pipeline_paper/submit_dirac_m360.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=debug
+#SBATCH -o optim_pot_m360.out  #Nombre del archivo de salida
+#SBATCH -J optim_pot_m360  #Nombre del trabajo
+#SBATCH --nodes=1  #Numero de nodos para correr el trabajo
+#SBATCH --ntasks=4  #Numero de procesos
+#SBATCH --tasks-per-node=4  #Numero de tareas por nodo
+
+#Prepara el ambiente de trabajo
+ulimit -l unlimited
+
+#Ejecuta el programa paralelo con MPI
+# . /etc/profile
+prun julia -p 4
