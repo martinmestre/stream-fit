876be18fe6abd42c2acf0ab2bcc24f65b7755361
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-08-28 15:31:11 -0300

    Update little

--- pipeline_paper/submit_dirac_m300.sh
+++ pipeline_paper/submit_dirac_m300.sh
@@ -2,9 +2,9 @@
 #SBATCH --partition=batch
 #SBATCH -o optim_pot_m300.out  #Nombre del archivo de salida
 #SBATCH -J optim_pot_m300  #Nombre del trabajo
-#SBATCH --nodes=3  #Numero de nodos para correr el trabajo
-#SBATCH --ntasks=3  #Numero de procesos
-#SBATCH --cpus-per-task=48  #Numero de cpus por proceso
+###SBATCH --nodes=3  #Numero de nodos para correr el trabajo
+#SBATCH --ntasks=144  #Numero de procesos
+#SBATCH --cpus-per-task=1  #Numero de cpus por proceso
 
 #Prepara el ambiente de trabajo
 ulimit -l unlimited
