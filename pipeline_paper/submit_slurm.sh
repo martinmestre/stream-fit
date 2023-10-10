d99d1ffb117619412dd35dbf67b538826d075643
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-10-10 16:05:26 -0300

    Largue corrida de test_slurm_manager.jl en serafin y eulogia

--- pipeline_paper/submit_slurm.sh
+++ pipeline_paper/submit_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --partition=short
-#SBATCH -o short.out  #Nombre del archivo de salida
+#SBATCH -o short_test.out  #Nombre del archivo de salida
 #SBATCH -J m56  #Nombre del trabajo
 #SBATCH --nodes=2  #Numero de nodos para correr el trabajo
-#SBATCH --ntasks-per-node=256  #Numero de procesos
+#SBATCH --ntasks-per-node=64  #Numero de procesos
 
 
 #Prepara el ambiente de trabajo
