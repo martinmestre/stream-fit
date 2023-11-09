2b8ba9882faca0ddcecf6e6523ab6aa4ecc6e6a7
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-11-09 18:42:38 -0300

    Wong Color palette

---
+++ pipeline_paper/wongcolors.jl
@@ -0,0 +1,11 @@
+function wongcolors()
+    return [
+        RGB(0/255, 114/255, 178/255), # blue
+        RGB(230/255, 159/255, 0/255), # orange
+        RGB(0/255, 158/255, 115/255), # green
+        RGB(204/255, 121/255, 167/255), # reddish purple
+        RGB(86/255, 180/255, 233/255), # sky blue
+        RGB(213/255, 94/255, 0/255), # vermillion
+        RGB(240/255, 228/255, 66/255), # yellow
+    ]
+end
